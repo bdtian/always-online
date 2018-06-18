0bec78f9a92c79ca8aca0e4345598ec0af6cc653
tianbaideng <tianbaideng@xiaoyezi.com>
2018-06-18 16:21:41 +0800

    VM-000: [feature] socket.io framework for msg sync
    Change-Id: I14d36bf26aa58e63138351923027298ba365964a

---
+++ index.js
@@ -0,0 +1,208 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+var io = require('socket.io')(http);
+var redisAdapter = require('socket.io-redis')
+
+var log4js = require('log4js');
+var redis = require('redis')
+
+// init logger
+log4js.configure({
+  appenders: {
+    console: {
+      type: 'stdout'
+    },
+    file: {
+      type: 'dateFile',
+      filename: 'logs/always-online_',
+      pattern: 'yyyy-MM-dd.log',
+      alwaysIncludePattern: true,
+      maxLogSize: 8 * 1024 * 1024,
+      backups: 4
+    }
+  },
+  categories: { default: { appenders: ['console', 'file'], level: 'trace' } }
+});
+
+const logger = log4js.getLogger();
+
+var redisClient = redis.createClient;
+
+// used for cluster sync 'server-msg'
+var pub = redisClient({ host: 'localhost', port: 6379 });
+var sub = redisClient({ host: 'localhost', port: 6379 });
+
+// used for socket.io msg sync.
+var adapter = redisAdapter({ host: 'localhost', port: 6379 });
+
+// used for msg storage.
+var redis = redisClient({ host: 'localhost', port: 6379 });
+
+
+var onlineUsers = {
+  //uid:{socket: socket}
+};
+
+var kickout = function(uid, sid) {
+  if (uid in onlineUsers) {
+    var socket = onlineUsers[uid].socket;
+    if (socket.id != sid) {
+      logger.warn("kickout: " + socket.id);
+      socket.emit('kickout', 0);
+      socket.disconnect();
+     }
+  }
+}
+
+var init = function() {
+  adapter.pubClient.on('error', function() {
+    logger.error('socket.io-redis pubClient error');
+  });
+  adapter.subClient.on('error', function() {
+    logger.error('socket.io-redis subClient error');
+  });
+
+  io.adapter(adapter);
+
+  sub.subscribe('server-msg')
+  sub.on('message', function(err, resp) {
+    // resp struct {'cmd': 'data':}
+    var data = JSON.parse(resp);
+    var cmd = data.cmd;
+    if (cmd == 'socket-connected') {
+      var uid = data.data.uid;
+      var sid = data.data.sid;
+      kickout(uid, sid);
+    }
+  });
+}
+
+// hook to connection message, and call auth handler.
+var authProcessor = function(io, auth, options, callback) {
+  var timeout;
+  if (typeof options === 'function') {
+    callback = options;
+    options = {};
+  }
+  options.timeout || (options.timeout = 1000);
+  timeout = function(time, fn) {
+    return setTimeout(fn, time);
+  };
+  return io.on('connection', function(socket) {
+    var ip = socket.handshake.address;
+    logger.info('recv a connection from ip: ' + ip + ", socket.id: " + socket.id);
+
+    var disconnect;
+    disconnect = function(error) {
+      if (error == null) {
+        error = 'unauthorized';
+      }
+      if (error instanceof Error) {
+        error = error.message;
+      }
+      socket.emit('authenticate', {status: 1, data: error});
+      return socket.disconnect();
+    };
+    timeout(options.timeout, function() {
+      if (!socket.authenticated) {
+        return disconnect('authentication timeout');
+      }
+    });
+    socket.authenticated = false;
+    return socket.on('authenticate', function(data) {
+      logger.debug('authenticate msg: ' + data);
+      return auth(socket, data, function(error) {
+        if (error != null) {
+          logger.warn('auth error');
+          return disconnect(error);
+        } else {
+          logger.debug('auth pass');
+          socket.authenticated = true;
+          socket.emit('authenticate', {status: 0});
+          return callback(socket);
+        }
+      });
+    });
+  });
+}
+
+var authHandler = function(socket, data, done) {
+  // check for valid credential data
+  var uid = data.uid;
+  var token = data.token;
+  redis.get(uid, function(err, reply) {
+      // reply is null when the key is missing
+      logger.debug(reply);
+      if (reply == token) {
+        socket.uid = uid;
+        done();
+      } else {
+        done(new Error('bad credentials'))
+      }
+  });
+};
+
+var postAuthHandler = function(socket) {
+  var uid = socket.uid;
+
+  pub.publish('server-msg',  JSON.stringify({cmd:'socket-connected', data: {uid: uid, sid: socket.id}}));
+  kickout(uid, socket.id);
+
+  onlineUsers[uid] = {socket: socket};
+
+  socket.on('join', function(msg){
+
+    logger.info('recv a join with msg: ' + msg);
+
+    var roomId = msg.roomId;
+    socket.rid = roomId;
+
+    if (roomId === '' || roomId == 'undefined') {
+       socket.emit('join',{'status': 1});
+       return;
+    }
+
+    socket.join(roomId, function() {
+      logger.info('join room success, roomId: ' + roomId);
+      socket.emit('join', {status: 0});
+      socket.to(roomId).emit('remoteJoin', {uid: socket.uid});
+    });
+  });
+
+  socket.on('leave', function(msg) {
+    socket.leave(socket.rid, function(msg) {
+      logger.info('leave room success, roomId: ' + roomId);
+    });
+  });
+
+  socket.on('msg', function(msg) {
+    logger.debug('recv a msg: ' + msg);
+    socket.to(socket.rid).emit('msg', msg);
+  });
+
+  socket.on('sync', function(msg) {
+    // load datbase,
+  });
+
+  socket.on('disconnect', function() {
+    delete onlineUsers[socket.uid];
+    logger.info('disconnect: ' + socket.id);
+  });
+};
+
+
+// main
+(function main() {
+  init();
+  authProcessor(io, authHandler, {timeout: 10000}, postAuthHandler);
+
+  http.listen(3000, function(){
+    logger.info('listening on *:3000');
+  });
+
+  app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+})();
